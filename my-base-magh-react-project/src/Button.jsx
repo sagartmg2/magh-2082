9e624effc100a3ec82e4a9d5322fcde132215856
dev.sagartmg <dev.sagartmg@gmail.com>
2026-04-11 09:07:32 +0545

    refactor:

--- my-base-magh-react-project/src/Button.jsx
+++ my-base-magh-react-project/src/Button.jsx
@@ -1,7 +1,23 @@
-function Button(props) {
+function Button({ label, children, size, rounded, className, ...rest }) {
+  let sizeClss = "px-6 py-2";
+  if (size == "sm") {
+    sizeClss = "px-2 py-1";
+  } else if (size == "lg") {
+    sizeClss = "px-8 py-4";
+  }
+
+  let classes = `hover:bg-amber-500 disabled:bg-amber-200 disabled:cursor-no-drop  capitalize   border border-gray-300 bg-amber-700 text-white ${rounded ? "rounded-4xl" : ""} ${sizeClss} `;
+
+  classes +=className;
+
   return (
-    <button className="hover:bg-amber-500 capitalize  px-6 py-2 border border-gray-300 bg-amber-700 text-white ">
-      {props.label}
+    <button
+      {...rest}
+      // onClick={onClick}
+      className={classes}
+    >
+      {label}
+      {children}
     </button>
   );
 }
--- my-base-magh-react-project/src/TodosCrud.jsx
+++ my-base-magh-react-project/src/TodosCrud.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useState } from "react";
+import Button, { RoundedButton, SmallButton } from "./Button";
+import { Pencil, Trash } from "lucide-react";
 
 export default function TodosCrud() {
   const [todos, setTodos] = useState(["react", "html"]);
@@ -8,17 +10,17 @@ export default function TodosCrud() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    let title = document.getElementById("title").value;
-    console.log(title);
-    console.log(title);
+    // let title = document.getElementById("title").value;
+
+    let title = e.target.title.value;
 
     // todos.push(title); // ERROR: cannot change state vairable directly
 
-    let oldTodos = [...todos]; // ERFERENCE variable
-    oldTodos.push(title);
-    console.log(oldTodos);
+    // let oldTodos = [...todos]; // ERFERENCE variable
+    // oldTodos.push(title);
+    // setTodos(oldTodos);
 
-    setTodos(oldTodos);
+    setTodos([...todos, title]);
   };
 
   return (
@@ -27,24 +29,63 @@ export default function TodosCrud() {
 
       <br></br>
       <br></br>
-      <form>
-        <input id="title" placeholder="title" className="border p-3" />
-        <button
-          onClick={(e) => {
-            handleSubmit(e);
-          }}
+      <form onSubmit={handleSubmit}>
+        <input
+          name="title"
+          id="title"
+          placeholder="title"
+          className="border p-3 disabled:bg-gray-200"
+        />
+        <Button
+          // type="button"
+          // type="submit"
+          // disabled
+          // onClick={(e) => {
+          //   handleSubmit(e);
+          // }}
+          size="lg"
+          rounded
         >
-          submit
-        </button>
+          <span>Add tag</span>
+        </Button>
+
+        {/* <Button size="ms" rounded>
+          <span>submit</span>
+        </Button>
+        <Button size="lg" rounded>
+          <span>submit</span>
+        </Button>
+        <Button rounded>
+          <span>submit</span>
+        </Button>
+
+        <Button label="submit" />
+        <SmallButton label="submit" />
+        <RoundedButton label="submit" /> */}
       </form>
       <br />
       <br />
 
       <ul className="list-disc pl-10">
         {todos.map((el) => {
-          return <li>{el} (completed)</li>;
+          return (
+            <li>
+              {el} (pending){" "}
+              <Button size="sm" className="bg-gray-400">
+                <Pencil className="inline" />
+                edit
+              </Button>
+              <Button size="sm" className="bg-red-600" onClick={() =>{}} >
+                <Trash className="inline" /> delete
+              </Button>
+            </li>
+          );
         })}
       </ul>
+
+      {/* <table>
+        <td>{el} (completed)</li>
+      </table> */}
     </div>
   );
 }
